c384e0b8e10cdebc228018ad46f0f40c757b4a4e
AndyBeable <andy.beable@gmail.com>
2021-03-20 22:01:06 +0000

    copy

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -24,10 +24,10 @@ function About() {
                   <h3>Hello!</h3>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor
+                    do eiusmod tempor incididunt ut labore et dolore magna some
+                    more text aliqua. Ut enim ad minim veniam, quis nostrud
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor
                   </p>
                   <a href={pdf} download>
                     <IoDocumentTextOutline />
