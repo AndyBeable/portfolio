40b6cc0d72457e08ae4ca16eeb0f01719066b00d
AndyBeable <andy.beable@gmail.com>
2021-03-30 15:53:23 +0100

    work aos

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import WorkItem from './WorkItem';
 import './Work.css';
 import { workObjOne, workObjTwo, workObjThree } from './Data';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 function Work() {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
+
   return (
     <>
-      <section className='work' id='work'>
+      <section data-aos='fade-up' className='work' id='work'>
         <div className='wrapper copy'>
           <h3>Work</h3>
           <h1>Latest Projects</h1>
